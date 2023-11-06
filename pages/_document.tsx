aff242f525d55b62830073517bebbe4fba64c176
양희준 <88187322+YHJ96@users.noreply.github.com>
2023-11-06 18:19:55 +0900

    ----

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,34 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>The Rock</title>
+        <meta name="description" content="The Rock Description" />
+
+        <meta
+          property="og:url"
+          content="https://next-og-test-rosy.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="The Rock" />
         <meta property="og:description" content="The Rock Description" />
         <meta
-          property="og:url"
-          content="https://www.imdb.com/title/tt0117500/"
+          property="og:image"
+          content="https://yaimg.yanolja.com/v5/2023/10/18/14/640/652fe6bfe3b9e4.23846689.jpg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="next-og-test-rosy.vercel.app"
         />
         <meta
-          property="og:image"
+          property="twitter:url"
+          content="https://next-og-test-rosy.vercel.app/"
+        />
+        <meta name="twitter:title" content="The Rock" />
+        <meta name="twitter:description" content="The Rock Description" />
+        <meta
+          name="twitter:image"
           content="https://yaimg.yanolja.com/v5/2023/10/18/14/640/652fe6bfe3b9e4.23846689.jpg"
         />
       </Head>
