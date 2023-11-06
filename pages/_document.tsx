b88502e1a06d8ee695417446e1544f2303aa8fd3
양희준 <88187322+YHJ96@users.noreply.github.com>
2023-11-06 18:56:58 +0900

    ㅁㄴㅇ

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <title>The Rock</title>
         <meta name="description" content="The Rock Description" />
 
-        <meta
+        {/* <meta
           property="og:url"
           content="https://next-og-test-rosy.vercel.app/"
         />
@@ -18,25 +18,9 @@ export default function Document() {
           content={`The Rock Description 
         The Rock Description
         The Rock Description`}
-        />
+        /> */}
         <meta
           property="og:image"
-          content="https://yaimg.yanolja.com/v5/2023/10/18/14/640/652fe6bfe3b9e4.23846689.jpg"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:domain"
-          content="next-og-test-rosy.vercel.app"
-        />
-        <meta
-          property="twitter:url"
-          content="https://next-og-test-rosy.vercel.app/"
-        />
-        <meta name="twitter:title" content="The Rock" />
-        <meta name="twitter:description" content="The Rock Description" />
-        <meta
-          name="twitter:image"
           content="https://yaimg.yanolja.com/v5/2023/10/18/14/640/652fe6bfe3b9e4.23846689.jpg"
         />
       </Head>
