2f2c39f5a0bfdd74830cd65fa25d91fa900a367a
양희준 <88187322+YHJ96@users.noreply.github.com>
2023-11-06 18:17:38 +0900

    []

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="The Rock" />
+        <meta property="og:description" content="The Rock Description" />
+        <meta
+          property="og:url"
+          content="https://www.imdb.com/title/tt0117500/"
+        />
+        <meta
+          property="og:image"
+          content="https://yaimg.yanolja.com/v5/2023/10/18/14/640/652fe6bfe3b9e4.23846689.jpg"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
